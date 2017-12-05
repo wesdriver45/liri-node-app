c89e2a79a3d09aa2c08fee2f09b0dd3b664fda0f
wesdriver45 <wesdriver45@gmail.com>
2017-12-04 20:40:30 -0500

    twitter keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<r5v3rSRP3D83IOlygOQYTe6aR>',
+  consumer_secret: '<t4mzFfRviMgcV5dvDnoFVlEQpqBXx9QvHqWSQzTfPBmlqzh7ND>',
+  access_token_key: '<937839878766817281-PiROymOPLOMQaJae3RmRkI2FTEyVwqR>',
+  access_token_secret: '<2CzINSxCWY7VYNbfSBKxd9CwRvugiKSCz1UOKqncUwHUW>',
+}
+
+module.exports = twitterKeys;
