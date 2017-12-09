c0763687e37a6b5ef38d079cef69d2a8fb76dad9
wesdriver45 <wesdriver45@gmail.com>
2017-12-08 22:32:22 -0500

    twitter function

--- liri.js
+++ liri.js
@@ -6,7 +6,9 @@ var keys = require("./keys.js");
 var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 
+//user input
 var userCommand = process.argv[2];
+var userInput = process.argv[3];
 
 //switch case function for commands
 var command = function(action) {
@@ -28,13 +30,35 @@ var command = function(action) {
 		break;
 
 		default:
-			console.log("Please enter a valid command.\n Choices: my-tweets, spotify-this-song, movie-this, do-what-it-says.");
+			console.log("Please enter a valid command.\nChoices: my-tweets, spotify-this-song, movie-this, do-what-it-says.");
+			break;
 	};
 };
 
 
-// //twitter function
-// var myTweets = function() {
-// 	var client = new twitter(keys.twitterKeys);
-// 	client.get(path, params, callback);
-// }
+//twitter function
+var myTweets = function() {
+	var client = new twitter(keys.twitterKeys);
+
+	var params = {screen_name: "Random_Namely", count: 20};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (error) {
+			console.log(error);
+
+			var data = [];
+			for (var i = 0; i < tweets.length; i++) {
+				data.push({
+					"Tweets: " :tweets[i].text,
+					"Created: " :tweets[i].created_at,
+					
+				})
+			}
+			console.log(data);		
+		}
+	});
+}
+
+//run the app
+command(userCommand, userInput);
+
