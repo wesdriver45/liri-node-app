b0a4c87725e77e54d4a83f585773212c5e172fc9
wesdriver45 <wesdriver45@gmail.com>
2017-12-08 20:45:10 -0500

    commands

--- liri.js
+++ liri.js
@@ -4,15 +4,37 @@ var request = require("request");
 var inquirer = require("inquirer");
 var keys = require("./keys.js");
 var twitter = require("twitter");
-var spotify = require("spotify");
+var spotify = require("node-spotify-api");
 
-//twitter function
-var myTweets = function() {
-	var client = new twitter(keys.twitterKeys);
-	client.get(path, params, callback);
+var userCommand = process.argv[2];
+
+//switch case function for commands
+var command = function(action) {
+	switch(action) {
+		case "my-tweets":
+		myTweets();
+		break;
+
+		case "spotify-this-song":
+		getSpotify();
+		break;
+
+		case "movie-this":
+		getMovie();
+		break;
+
+		case "do-what-it-says":
+		doThis();
+		break;
+
+		default:
+			console.log("Please enter a valid command.\n Choices: my-tweets, spotify-this-song, movie-this, do-what-it-says.");
+	};
+};
 
 
-
-
-
-}
+// //twitter function
+// var myTweets = function() {
+// 	var client = new twitter(keys.twitterKeys);
+// 	client.get(path, params, callback);
+// }
